833ed3006ec41269af6320877fa72bb0775bd3e1
Chin <chin.bimbo@gmail.com>
2013-12-08 17:46:05 -0500

    more efficient

--- script.js
+++ script.js
@@ -11,60 +11,54 @@
 // @run-at       document-end
 // ==/UserScript==
 
-var main = function() {
-    function walk(node) {      
-        var child, next;
-        switch (node.nodeType)  {
-            case 1:  // Element
-            case 9:  // Document
-            case 11: // Document fragment
-                child = node.firstChild;
-                while (child) {
-                    next = child.nextSibling;
-                    walk(child);
-                    child = next;
-                }
+
+function walk(node) {      
+    var child, next;
+    switch (node.nodeType)  {
+        case 1:  // Element
+        case 9:  // Document
+        case 11: // Document fragment
+            child = node.firstChild;
+            while (child) {
+                next = child.nextSibling;
+                walk(child);
+                child = next;
+            }
+            break;
+        case 3: // Text node
+                handleText(node);
                 break;
-            case 3: // Text node
-                    handleText(node);
-                    break;
-        }
     }
-
-    var laughing = "<span class='myemo laughing'></span>";
-    var crying   = "<span class='myemo crying'></span>";
-    var ohgoon   = "<span class='myemo ohgoon'></span>";
-    var rolling  = "<span class='myemo rolling'></span>";
-    var hehe     = "<span class='myemo hehe'></span>";
-
-    function handleText(textNode) {
-        if (textNode.nodeValue.match(/(:\)\))|(=\)\))|(;\)\))|(:\(\()|(:-j)/)) {
-            var parent = textNode.parentNode;
-            var replacement = "<span class='myemo laughing'></span>";  
-            textNode.nodeValue = textNode.nodeValue.replace(/:\)\)/g, laughing);
-            textNode.nodeValue = textNode.nodeValue.replace(/=\)\)/g, rolling);
-            textNode.nodeValue = textNode.nodeValue.replace(/;\)\)/g, hehe);
-            textNode.nodeValue = textNode.nodeValue.replace(/:\(\(/g, crying);
-            textNode.nodeValue = textNode.nodeValue.replace(/:-j/g, ohgoon);
-            var newSpan = document.createElement('span');
-            newSpan.innerHTML = textNode.nodeValue;
-            parent.appendChild(newSpan);
-            textNode.parentNode.removeChild(textNode);
-        }
-    }
-
-    var fileref=document.createElement("link");
-    fileref.setAttribute("rel", "stylesheet");
-    fileref.setAttribute("type", "text/css");
-    fileref.setAttribute("href", "https://rawgithub.com/chinhodado/ym_emo_fb/master/emo_style.css");
-    document.getElementsByTagName("head")[0].appendChild(fileref);
-                   
-    walk(document.body);
 }
 
-var script = document.createElement('script');
-script.type = "text/javascript";
-script.textContent = '(' + main.toString() + ')();';
-document.body.appendChild(script);
+var laughing = "<span class='myemo laughing'></span>";
+var crying   = "<span class='myemo crying'></span>";
+var ohgoon   = "<span class='myemo ohgoon'></span>";
+var rolling  = "<span class='myemo rolling'></span>";
+var hehe     = "<span class='myemo hehe'></span>";
 
-document.addEventListener('DOMNodeInserted', function() { main() }, false);
+function handleText(textNode) {
+    if (textNode.nodeValue.match(/(:\)\))|(=\)\))|(;\)\))|(:\(\()|(:-j)/)) {
+        var parent = textNode.parentNode;
+        var replacement = "<span class='myemo laughing'></span>";  
+        textNode.nodeValue = textNode.nodeValue.replace(/:\)\)/g, laughing);
+        textNode.nodeValue = textNode.nodeValue.replace(/=\)\)/g, rolling);
+        textNode.nodeValue = textNode.nodeValue.replace(/;\)\)/g, hehe);
+        textNode.nodeValue = textNode.nodeValue.replace(/:\(\(/g, crying);
+        textNode.nodeValue = textNode.nodeValue.replace(/:-j/g, ohgoon);
+        var newSpan = document.createElement('span');
+        newSpan.innerHTML = textNode.nodeValue;
+        parent.appendChild(newSpan);
+        textNode.parentNode.removeChild(textNode);
+    }
+}
+
+var fileref=document.createElement("link");
+fileref.setAttribute("rel", "stylesheet");
+fileref.setAttribute("type", "text/css");
+fileref.setAttribute("href", "https://rawgithub.com/chinhodado/ym_emo_fb/master/emo_style.css");
+document.getElementsByTagName("head")[0].appendChild(fileref);
+               
+walk(document.body);
+
+document.addEventListener('DOMNodeInserted', function() { walk(document.body) }, false);
