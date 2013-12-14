66d9d3d17a9d4581c7aefbbd81d4e45a9a7ab52e
Chin <chin.bimbo@gmail.com>
2013-12-13 20:40:56 -0500

    update css and script

--- script.js
+++ script.js
@@ -36,16 +36,35 @@ var crying   = "<span class='myemo crying'></span>";
 var ohgoon   = "<span class='myemo ohgoon'></span>";
 var rolling  = "<span class='myemo rolling'></span>";
 var hehe     = "<span class='myemo hehe'></span>";
+var angry    = "<span class='myemo angry'></span>";
+var batting  = "<span class='myemo batting'></span>";
+var blushing = "<span class='myemo blushing'></span>";
+var broken   = "<span class='myemo broken'></span>";
+var devil    = "<span class='myemo devil'></span>";
+var hug      = "<span class='myemo hug'></span>";
+var love     = "<span class='myemo love'></span>";
+var smug     = "<span class='myemo smug'></span>";
+var whew     = "<span class='myemo whew'></span>";
+var worried  = "<span class='myemo worried'></span>";
 
 function handleText(textNode) {
-    if (textNode.nodeValue.match(/(:\)\))|(=\)\))|(;\)\))|(:\(\()|(:-j)/)) {
+    if (textNode.nodeValue.match(/:\)\)|:\(\(|:-j|=\)\)|;\)\)|x\(|;;\)|:">|=\(\(|>:\(|>:D<|:x|:>|#:-s|:-s/i)) {
         var parent = textNode.parentNode;
-        var replacement = "<span class='myemo laughing'></span>";  
         textNode.nodeValue = textNode.nodeValue.replace(/:\)\)/g, laughing);
-        textNode.nodeValue = textNode.nodeValue.replace(/=\)\)/g, rolling);
-        textNode.nodeValue = textNode.nodeValue.replace(/;\)\)/g, hehe);
         textNode.nodeValue = textNode.nodeValue.replace(/:\(\(/g, crying);
         textNode.nodeValue = textNode.nodeValue.replace(/:-j/g, ohgoon);
+        textNode.nodeValue = textNode.nodeValue.replace(/=\)\)/g, rolling);
+        textNode.nodeValue = textNode.nodeValue.replace(/;\)\)/g, hehe);
+        textNode.nodeValue = textNode.nodeValue.replace(/x\(/gi, angry);
+        textNode.nodeValue = textNode.nodeValue.replace(/;;\)/g, batting);
+        textNode.nodeValue = textNode.nodeValue.replace(/:">/g, blushing);
+        textNode.nodeValue = textNode.nodeValue.replace(/=\(\(/g, broken);
+        textNode.nodeValue = textNode.nodeValue.replace(/>:\(/g, devil);
+        textNode.nodeValue = textNode.nodeValue.replace(/>:D</gi, hug);
+        textNode.nodeValue = textNode.nodeValue.replace(/:x/gi, love);
+        textNode.nodeValue = textNode.nodeValue.replace(/:>/g, smug);
+        textNode.nodeValue = textNode.nodeValue.replace(/#:-S/gi, whew);
+        textNode.nodeValue = textNode.nodeValue.replace(/:-s/gi, worried);
         var newSpan = document.createElement('span');
         newSpan.innerHTML = textNode.nodeValue;
         parent.replaceChild(newSpan, textNode);
