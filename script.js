891c880fb641b76cf2a75ec1ae262d27fa59087e
Trung Do <chin.bimbo@gmail.com>
2024-12-22 20:35:32 -0500

    Update with all emojis
    Add new/missing Yahoo emojis. Now contains all 119 Yahoo emojis.
    
    Refactor the code
     - no need to manually list all CSS classes
     - no need to manually list all replacement spans
     - no need to manually build the regex
     - Use img directly instead of span with background
     - add index.html to show all emojis

--- script.js
+++ script.js
@@ -27,159 +27,211 @@ function walk(node) {
             }
             break;
         case 3: // Text node
-                handleText(node);
-                break;
+            handleText(node);
+            break;
     }
 }
 
-const laughing     = "<span class='myemo laughing'></span>";
-const crying       = "<span class='myemo crying'></span>";
-const ohgoon       = "<span class='myemo ohgoon'></span>";
-const rolling      = "<span class='myemo rolling'></span>";
-const hehe         = "<span class='myemo hehe'></span>";
-const angry        = "<span class='myemo angry'></span>";
-const batting      = "<span class='myemo batting'></span>";
-const blushing     = "<span class='myemo blushing'></span>";
-const broken       = "<span class='myemo broken'></span>";
-const devil        = "<span class='myemo devil'></span>";
-const hug          = "<span class='myemo hug'></span>";
-const love         = "<span class='myemo love'></span>";
-const smug         = "<span class='myemo smug'></span>";
-const whew         = "<span class='myemo whew'></span>";
-const worried      = "<span class='myemo worried'></span>";
-const bye          = "<span class='myemo bye'></span>";
-const callme       = "<span class='myemo callme'></span>";
-const clown        = "<span class='myemo clown'></span>";
-const daydreaming  = "<span class='myemo daydreaming'></span>";
-const donttell     = "<span class='myemo donttell'></span>";
-const loser        = "<span class='myemo loser'></span>";
-const nerd         = "<span class='myemo nerd'></span>";
-const notalking    = "<span class='myemo notalking'></span>";
-const phone        = "<span class='myemo phone'></span>";
-const raised       = "<span class='myemo raised'></span>";
-const sick         = "<span class='myemo sick'></span>";
-const silly        = "<span class='myemo silly'></span>";
-const sleepy       = "<span class='myemo sleepy'></span>";
-const straight     = "<span class='myemo straight'></span>";
-const talkhand     = "<span class='myemo talkhand'></span>";
-const timeout      = "<span class='myemo timeout'></span>";
-const witsend      = "<span class='myemo witsend'></span>";
-const applause     = "<span class='myemo applause'></span>";
-const beatup       = "<span class='myemo beatup'></span>";
-const bringit      = "<span class='myemo bringit'></span>";
-const dancing      = "<span class='myemo dancing'></span>";
-const doh          = "<span class='myemo doh'></span>";
-const dontknow     = "<span class='myemo dontknow'></span>";
-const dontsee      = "<span class='myemo dontsee'></span>";
-const drooling     = "<span class='myemo drooling'></span>";
-const frustrated   = "<span class='myemo frustrated'></span>";
-const hipno        = "<span class='myemo hipno'></span>";
-const liar         = "<span class='myemo liar'></span>";
-const money        = "<span class='myemo money'></span>";
-const nail         = "<span class='myemo nail'></span>";
-const notworthy    = "<span class='myemo notworthy'></span>";
-const party        = "<span class='myemo party'></span>";
-const peace        = "<span class='myemo peace'></span>";
-const phbbt        = "<span class='myemo phbbt'></span>";
-const praying      = "<span class='myemo praying'></span>";
-const rockon       = "<span class='myemo rockon'></span>";
-const shame        = "<span class='myemo shame'></span>";
-const sigh         = "<span class='myemo sigh'></span>";
-const thinking     = "<span class='myemo thinking'></span>";
-const thumbdown    = "<span class='myemo thumbdown'></span>";
-const thumbup      = "<span class='myemo thumbup'></span>";
-const waiting      = "<span class='myemo waiting'></span>";
-const whistling    = "<span class='myemo whistling'></span>";
-const yawn         = "<span class='myemo yawn'></span>";
+const emojis = {
+    base: {
+        happy: ['Happy', `:)`],
+        sad: ['Sad', `:(`],
+        winking: ['Winking', `;)`],
+        grin: ['Big grin', `:D`],
+        batting: ['Batting eyelashes', `;;)`],
+        confused: ['Confused', `:-/`],
+        love: ['Love struck', `:x`],
+        blushing: ['Blushing', `:">`],
+        tongue: ['Tongue', `:P`],
+        kiss: ['Kiss', `:-*`],
+        surprised: ['Surprised', `:-O`],
+        angry: ['Angry', `x(`],
+        smug: ['Smug', `:>`],
+        cool: ['Cool', `B-)`],
+        worried: ['Worried', `:-s`],
+        devil: ['Devil', `>:)`],
+        crying: ['Crying', `:((`],
+        laughing: ['Laughing', `:))`],
+        straight: ['Straight face', `:|`],
+        raised: ['Raised eyebrows', `/:)`],
+        angel: ['Angel', `O:-)`],
+        nerd: ['Nerd', `:-B`],
+        talkhand: ['Talk to the hand', `=;`],
+        sleepy: ['Sleepy', `I-)`],
+        rollingeyes: ['Rolling eyes', `8-|`],
+        sick: ['Sick', `:-&`],
+        donttell: ["Don't tell", `:-$`],
+        notalking: ['No talking', `[-(`],
+        clown: ['Clown', `:O)`],
+        silly: ['Silly', `8-}`],
+        yawn: ['Yawn', `(:|`],
+        drooling: ['Drooling', `=P~`],
+        thinking: ['Thinking', `:-?`],
+        doh: ["D'oh", `#-o`],
+        applause: ['Applause', `=D>`],
+        pig: ['Pig', `:@)`],
+        cow: ['Cow', `3:-O`],
+        monkey: ['Monkey', `:(|)`],
+        chicken: ['Chicken', `~:>`],
+        rose: ['Rose', `@};-`],
+        goodluck: ['Good luck', `%%-`],
+        flag: ['Flag', `**==`],
+        pumpkin: ['Pumpkin', `(~~)`],
+        coffee: ['Coffee', `~O)`],
+        idea: ['Idea', `*-:)`],
+        skull: ['Skull', `8-X`],
+        bug: ['Bug', `=:)`],
+        alien: ['Alien', `>-)`],
+        frustrated: ['Frustrated', `:-L`],
+        cowboy: ['Cowboy', `<):)`],
+        praying: ['Praying', `[-O<`],
+        hipno: ['Hypnotized', `@-)`],
+        money: ['Money eyes', `$-)`],
+        whistling: ['Whistling', `:-"`],
+        liar: ['Liar', `:^o`],
+        beatup: ['Punch', `b-(`],
+        peace: ['Peace sign', `:)>-`],
+        shame: ['Shame on you', `[-X`],
+        dancing: ['Dancing', `\\:D/`],
+        hug: ['Big hug', `>:D<`],
+    },
+    fighter: {
+        hiro: ['Hiro', `o->`],
+        billy: ['Billy', `o=>`],
+        april: ['April', `o-+`],
+        yinyang: ['Yinyang', `(%)`],
+    },
+    version6: {
+        broken: ['Broken heart', `=((`],
+        whew: ['Whew!', `#:-S`],
+        rolling: ['Rolling on the floor', `=))`],
+        loser: ['Loser', `L-)`],
+        party: ['Party', `<:-P`],
+        nail: ['Nail biting', `:-ss`],
+        waiting: ['Waiting', `:-w`],
+        sigh: ['Sigh', `:-<`],
+        phbbt: ['phbbbbt', `>:P`],
+        bringit: ['Bring it on', `>:/`],
+        hehe: ['Hee hee', `;))`],
+        chatterbox: ['Chatterbox', `:-@`],
+        notworthy: ['Not worthy', `^:)^`],
+        ohgoon: ['Oh go on', `:-j`],
+        star: ['Star', `(*)`],
+    },
+    version7: {
+        phone: ['On the phone', `:)]`],
+        callme: ['Call me', `:-c`],
+        witsend: ["At wit's end", `~X(`],
+        bye: ['Wave', `:-h`],
+        timeout: ['Timeout', `:-t`],
+        daydreaming: ['Daydreaming', `8->`],
+        dontknow: ["I don't know", `:-??`],
+        notlistening: ['Not listening', `%-(`],
+    },
+    version8: {
+        puppy: ['Puppy', `:o3`],
+    },
+    version9: {
+        dontsee: ["I don't want to see", `X_X`],
+        hurryup: ['Hurry up', `:!!`],
+        rockon: ['Rock on', `\\m/`],
+        thumbdown: ['Thumbs down', `:-q`],
+        thumbup: ['Thumbs up', `:-bd`],
+        wasnotme: ["It wasn't me!", `^#(^`],
+        bee: ['Bee', `:bz`],
+    },
+    version11: {
+        cheer: ['Cheer', `~^o^~`],
+        dizzy: ['Dizzy', `'@^@|||`],
+        cook: ['Cook', `[]---`],
+        eat: ['Eat', `^o^||3`],
+        giveup: ['Give up', `:-(||>`],
+        cold: ['Cold', `'+_+`],
+        hot: ['Hot', `:::^^:::`],
+        music: ['Music', `o|^_^|o`],
+        vomit: ['Vomit', `:puke!`],
+        sing: ['Sing', `o|\\~`],
+        catch: ['Catch', `o|:-)`],
+        exercise: ['Exercise', `[]==[]`],
+        highfive: ['High Five!', `:-)/\\:-)`],
+        gaming: ['Gaming', `:(game)`],
+        searchme: ['Search me', `'@-@`],
+        spooky: ['Spooky', `:->~~`],
+        studying: ['Studying', `?@_@?`],
+        tv: ['TV', `:(tv)`],
+        gift: ['Gift', `&[]`],
+        unlucky: ['Unlucky', `%||:-{`],
+        downonluck: ['Down on luck', `%*-{`],
+        fight: ['Fight', `:(fight)`],
+    },
+    web: {
+        pirate: ['Pirate', `:ar!`],
+        transformer: ['Transformer', `[..]`],
+    }
+}
+
+const allEmojis = [];
+for (let category in emojis) {
+    for (let emoji in emojis[category]) {
+        allEmojis.push([emoji, ...emojis[category][emoji]]);
+    }
+}
+
+const emojiMap = {};
+for (const emoji of allEmojis) {
+    emojiMap[emoji[2].toLowerCase()] = emoji;
+}
+
+allEmojis.sort((a, b) => b[2].length - a[2].length);
+let metachars = /[[\]{}()*+?.\\|^$\-,&#\s]/g;
+let patterns = [];
+for (let emoji of allEmojis) {
+    patterns.push('(' + emoji[2].replace(metachars, "\\$&") + ')');
+}
+let regexp = new RegExp(patterns.join('|'), 'gi');
+var ymEmoFbEnv;
 
 function handleText(textNode) {
-    if (textNode.nodeValue.match(/:\)\)|:\(\(|:-j|=\)\)|=]]|;\)\)|x\(|;;\)|:">|=\(\(|>:\)|>:D<|:x|:>|#:-s|:-s|:-h|:-c|:O\)|8->|:-\$|L-\)|:-B|\[-\(|:\)\]|\/:\)|:-&|8-\}|I-\)|:\||=;|:-t|~X\(|=D>|b-\(|>:\/|\\:D\/|#-o|:-\?\?|X_X|=P~|:-L|@-\)|:\^o|\$-\)|:-ss|\^:\)\^|<:-P|:\)>-|>:P|\[-O<|\\m\/|\[-X|:-<|:-\?|:-q|:-bd|:-w|:-"|\(:\|/i)) {
+    if (textNode.nodeValue.match(regexp)) {
         const parent = textNode.parentNode;
-        textNode.nodeValue = textNode.nodeValue.replace(/:-ss/gi, nail);
-        textNode.nodeValue = textNode.nodeValue.replace(/:-bd/gi, thumbup);
-        textNode.nodeValue = textNode.nodeValue.replace(/\(:\|/gi, yawn);
-        textNode.nodeValue = textNode.nodeValue.replace(/:\)\)/g, laughing);
-        textNode.nodeValue = textNode.nodeValue.replace(/:\(\(/g, crying);
-        textNode.nodeValue = textNode.nodeValue.replace(/:-j/g, ohgoon);
-        textNode.nodeValue = textNode.nodeValue.replace(/=\)\)/g, rolling);
-        textNode.nodeValue = textNode.nodeValue.replace(/=]]/g, rolling);
-        textNode.nodeValue = textNode.nodeValue.replace(/;\)\)/g, hehe);
-        textNode.nodeValue = textNode.nodeValue.replace(/~X\(/gi, witsend);
-        textNode.nodeValue = textNode.nodeValue.replace(/x\(/gi, angry);
-        textNode.nodeValue = textNode.nodeValue.replace(/;;\)/g, batting);
-        textNode.nodeValue = textNode.nodeValue.replace(/:">/g, blushing);
-        textNode.nodeValue = textNode.nodeValue.replace(/=\(\(/g, broken);
-        textNode.nodeValue = textNode.nodeValue.replace(/>:\)/g, devil);
-        textNode.nodeValue = textNode.nodeValue.replace(/>:D</gi, hug);
-        textNode.nodeValue = textNode.nodeValue.replace(/:x/gi, love);
-        textNode.nodeValue = textNode.nodeValue.replace(/:>/g, smug);
-        textNode.nodeValue = textNode.nodeValue.replace(/#:-S/gi, whew);
-        textNode.nodeValue = textNode.nodeValue.replace(/:-s/gi, worried);
-        textNode.nodeValue = textNode.nodeValue.replace(/:-h/gi, bye);
-        textNode.nodeValue = textNode.nodeValue.replace(/:-c/gi, callme);
-        textNode.nodeValue = textNode.nodeValue.replace(/:O\)/gi, clown);
-        textNode.nodeValue = textNode.nodeValue.replace(/8->/gi, daydreaming);
-        textNode.nodeValue = textNode.nodeValue.replace(/:-\$/gi, donttell);
-        textNode.nodeValue = textNode.nodeValue.replace(/L-\)/gi, loser);
-        textNode.nodeValue = textNode.nodeValue.replace(/:-B/gi, nerd);
-        textNode.nodeValue = textNode.nodeValue.replace(/\[-\(/gi, notalking);
-        textNode.nodeValue = textNode.nodeValue.replace(/:\)\]/gi, phone);
-        textNode.nodeValue = textNode.nodeValue.replace(/\/:\)/gi, raised);
-        textNode.nodeValue = textNode.nodeValue.replace(/:-&/gi, sick);
-        textNode.nodeValue = textNode.nodeValue.replace(/8-\}/gi, silly);
-        textNode.nodeValue = textNode.nodeValue.replace(/I-\)/gi, sleepy);
-        textNode.nodeValue = textNode.nodeValue.replace(/:\|/gi, straight);
-        textNode.nodeValue = textNode.nodeValue.replace(/=;/gi, talkhand);
-        textNode.nodeValue = textNode.nodeValue.replace(/:-t/gi, timeout);
-        textNode.nodeValue = textNode.nodeValue.replace(/=D>/gi, applause);
-        textNode.nodeValue = textNode.nodeValue.replace(/b-\(/gi, beatup);
-        textNode.nodeValue = textNode.nodeValue.replace(/>:\//gi, bringit);
-        textNode.nodeValue = textNode.nodeValue.replace(/\\:D\//gi, dancing);
-        textNode.nodeValue = textNode.nodeValue.replace(/#-o/gi, doh);
-        textNode.nodeValue = textNode.nodeValue.replace(/:-\?\?/gi, dontknow);
-        textNode.nodeValue = textNode.nodeValue.replace(/X_X/gi, dontsee);
-        textNode.nodeValue = textNode.nodeValue.replace(/=P~/gi, drooling);
-        textNode.nodeValue = textNode.nodeValue.replace(/:-L/gi, frustrated);
-        textNode.nodeValue = textNode.nodeValue.replace(/@-\)/gi, hipno);
-        textNode.nodeValue = textNode.nodeValue.replace(/:\^o/gi, liar);
-        textNode.nodeValue = textNode.nodeValue.replace(/\$-\)/gi, money);        
-        textNode.nodeValue = textNode.nodeValue.replace(/\^:\)\^/gi, notworthy);
-        textNode.nodeValue = textNode.nodeValue.replace(/<:-P/gi, party);
-        textNode.nodeValue = textNode.nodeValue.replace(/:\)>-/gi, peace);
-        textNode.nodeValue = textNode.nodeValue.replace(/>:P/gi, phbbt);
-        textNode.nodeValue = textNode.nodeValue.replace(/\[-O</gi, praying);
-        textNode.nodeValue = textNode.nodeValue.replace(/\\m\//gi, rockon);
-        textNode.nodeValue = textNode.nodeValue.replace(/\[-X/gi, shame);
-        textNode.nodeValue = textNode.nodeValue.replace(/:-</gi, sigh);
-        textNode.nodeValue = textNode.nodeValue.replace(/:-\?/gi, thinking);
-        textNode.nodeValue = textNode.nodeValue.replace(/:-q/gi, thumbdown);        
-        textNode.nodeValue = textNode.nodeValue.replace(/:-w/gi, waiting);
-        textNode.nodeValue = textNode.nodeValue.replace(/:-"/gi, whistling);
+        let newTxt = textNode.nodeValue.replace(regexp, function(code) {
+            let emoji = emojiMap[code.toLowerCase()];
+            if (ymEmoFbEnv !== undefined) {
+                return `images/${emoji[0]}.gif`;
+            }
+            else {
+                return chrome.runtime.getURL(`images/${emoji[0]}.gif`);
+            }
+        });
 
-        const newSpan = document.createElement('span');
-        newSpan.innerHTML = textNode.nodeValue;
-        parent.replaceChild(newSpan, textNode);
+        const newImg = document.createElement("img");
+        newImg.src = newTxt;
+        newImg.alt = '';
+        newImg.classList.add('ym_emo_fb');
+        parent.replaceChild(newImg, textNode);
     }
 }
 
-walk(document.body);
+if (ymEmoFbEnv === undefined) {
+    walk(document.body);
 
-// select the target node for mutation observation
-const target = document.body;
+    // select the target node for mutation observation
+    const target = document.body;
 
-// create an observer instance
-const observer = new MutationObserver(function (mutations) { // mutations: an array of MutationRecord objects
-    mutations.forEach(function (mutation) {
-        const addedList = mutation.addedNodes;
-        for (let i = 0; i < addedList.length; ++i) {
-            const item = addedList[i];  // Calling myNodeList.item(i) isn't necessary in JavaScript
-            walk(item);
-        }
+    // create an observer instance
+    const observer = new MutationObserver(function (mutations) { // mutations: an array of MutationRecord objects
+        mutations.forEach(function (mutation) {
+            const addedList = mutation.addedNodes;
+            for (let i = 0; i < addedList.length; ++i) {
+                const item = addedList[i];  // Calling myNodeList.item(i) isn't necessary in JavaScript
+                walk(item);
+            }
+        });
     });
-});
 
-// configuration of the observer:
-const config = {attributes: true, childList: true, characterData: true, subtree: true};
+    // configuration of the observer:
+    const config = {attributes: true, childList: true, characterData: true, subtree: true};
 
-// pass in the target node, as well as the observer options
-observer.observe(target, config);
+    // pass in the target node, as well as the observer options
+    observer.observe(target, config);
+}
